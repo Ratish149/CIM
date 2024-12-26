b8615a43192e67e5a39906577fc748c8daf1eb6a
Ratish149 <ratish.shakya149@gmail.com>
2024-12-26 15:18:20 +0545

    asd

--- CIM/settings.py
+++ CIM/settings.py
@@ -96,10 +96,21 @@ WSGI_APPLICATION = 'CIM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cim',
+        'USER': 'cim',
+        'PASSWORD': 'ratish123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
