0e3dd7b721fe7bd8b425b9862c0d0624970e2033
Ratish149 <ratish.shakya149@gmail.com>
2025-01-15 14:16:29 +0545

    dfdf

--- koshi_quality_standard/admin.py
+++ koshi_quality_standard/admin.py
@@ -3,6 +3,11 @@ from .models import Question,Requirement,Response
 from unfold.admin import ModelAdmin
 
 # Register your models here.
-admin.site.register(Question,ModelAdmin)
+class QuestionAdmin(ModelAdmin):
+    list_display = ('text', 'points')
+    search_fields = ('text',)
+    list_filter = ('points',)
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Requirement,ModelAdmin)
 admin.site.register(Response,ModelAdmin)
