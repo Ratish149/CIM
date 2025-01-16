45bed2572d3459155c9228b9f1281b770f430b2d
Ratish149 <ratish.shakya149@gmail.com>
2025-01-16 14:40:33 +0545

    dfd

--- CIM/settings.py
+++ CIM/settings.py
@@ -96,14 +96,14 @@ WSGI_APPLICATION = 'CIM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'cim',
@@ -112,7 +112,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
     }
-} """
+}
 
 
 # Password validation
