c3b53dbfc467c2ef6ca45b8c285e779f41409b78
Ratish149 <ratish.shakya149@gmail.com>
2025-01-20 11:26:10 +0545

    ddsd

--- mero_desh_merai_utpadan/urls.py
+++ mero_desh_merai_utpadan/urls.py
@@ -10,9 +10,9 @@ from .views import (
 
 urlpatterns = [
     path('nature-of-industry-category/', NatureOfIndustryCategoryListCreateView.as_view(), name='nature-of-industry-category-list-create'),
-    path('nature-of-industry-subcategory/', NatureOfIndustrySubCategoryListCreateView.as_view(), name='nature-of-industry-subcategory-list-create'),
+    path('nature-of-industry-sub-category/', NatureOfIndustrySubCategoryListCreateView.as_view(), name='nature-of-industry-sub-category-list-create'),
     path('mero-desh-merai-utpadan/', MeroDeshMeraiUtpadanListCreateView.as_view(), name='mero-desh-merai-utpadan-list-create'),
-    path('mero-desh-merai-utpadan/<int:pk>/', MeroDeshMeraiUtpadanRetrieveUpdateDestroyView.as_view(), name='mero-desh-merai-utpadan-detail'),
-    path('contact-form/', ContactFormListCreateView.as_view(), name='contact-form-list-create'),
+    path('mero-desh-merai-utpadan/<int:pk>/', MeroDeshMeraiUtpadanRetrieveUpdateDestroyView.as_view(), name='merodeshmeraiutpadan-detail'),
+    path('contact-form-submit/', ContactFormListCreateView.as_view(), name='contact-form-submit'),
     path('<int:pk>/status/', ApproveStatusView.as_view(), name='approve-status'),
 ]
