d5cf3ecfa035a78a280a7379ad28675d2e108c17
Ratish149 <ratish.shakya149@gmail.com>
2025-01-23 12:30:18 +0545

    dfd

--- koshi_quality_standard/urls.py
+++ koshi_quality_standard/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('calculate-points/', CalculatePointsView.as_view(), name='calculate-points'),  # POST: Submit answers and calculate points
     path('upload-requirements/', RequirementQuestionBulkUploadView.as_view(), name='upload-requirements'),
     path('report/<int:id>/', ResponseDetailView.as_view(), name='response-detail'),
-    path('contact-submit/',ContactFormListCreateView.as_view(),name='contact-form'),
+    path('contact-form-submit/',ContactFormListCreateView.as_view(),name='contact-form-submit'),
 
 ]
